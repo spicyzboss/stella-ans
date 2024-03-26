eceb7d0936e829c3880cdcf9fcd5c18511ca1484
spicyzboss <supachai@spicyz.io>
2024-03-27 04:17:29 +0700

    chore: add comments

--- calculate_reward.py
+++ calculate_reward.py
@@ -27,37 +27,54 @@ format_to_three_decimal_points = lambda a: (a[0], round(a[1], 3))
 apply_filter = lambda f: lambda a: list(filter(f, a))
 
 def calculate_reward(events: list[tuple[str, int, int]]) -> dict[str, float]:
+    # Initialize current timestamp
     CURRENT_TIMESTAMP = 0
 
+    # Filter unwanted events
     events = apply_filter(invalid_user)(events)
     events = apply_filter(invalid_timestamp)(events)
     events = apply_filter(invalid_share)(events)
 
+    # Sort events by timestamp
     events.sort(key=order_by_timestamp)
     
+    # Initialize user shares and balances
     user_shares = dict()
     user_balances = dict()
 
+    # Loop through each timestamp until timestamp is equal to max timestamp
     while CURRENT_TIMESTAMP < MAX_TIMESTAMP:
+        # Update user shares
         for event in events:
             user, timestamp, share = event
 
+            # Update user shares if timestamp is current timestamp
             if timestamp == CURRENT_TIMESTAMP:
+                # Update user share if user exists
                 if user_shares.get(user) is not None:
                     user_shares[user] = max(0, user_shares[user]+share)
+                # Create new user share if user does not exist
                 elif is_greater_than_zero(share):
                     user_shares.setdefault(user, share)
                     user_balances.setdefault(user, 0)
 
+        # Calculate sum of user shares in current timestamp
         sum_shares = sum(user_shares.values())
 
+        # Check if user shares is able to distribute
         if is_greater_than_zero(sum_shares):
+            # Distribute USDT to users
             for user in user_shares:
+                # Distribute USDT to user based on shares and distribution each timestamp
                 user_balances[user] += user_shares[user] * DISTRIBUTION_EACH_TIMESTAMP / sum_shares
 
+        # Remove events which is past current timestamp
         events = apply_filter(is_timestamp_greater(CURRENT_TIMESTAMP))(events)
+
+        # Increment current timestamp
         CURRENT_TIMESTAMP += 1
 
+    # Format user balances to three decimal points then return
     return dict(map(format_to_three_decimal_points, user_balances.items()))
 
 if __name__ == '__main__':
